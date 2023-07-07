051c910801038c70b61660b34f3588446de1a9fa
SuzanaSSP <suzanasp1026@gmail.com>
2023-07-07 17:22:09 -0600

    finished!

--- agent.py
+++ agent.py
@@ -1,9 +1,12 @@
 import numpy as np
 import random
+import math
 
 class Penguin:
     dt = 0.1
-    radius = 1
+    L = 50
+    midway_radius = L//2
+    center = [L//2, L//2]
 
     def __init__(self, x, y):
         self.x = x
@@ -15,15 +18,23 @@ class Penguin:
         self.change_c = np.array([self.change_x, self.change_y])
 
         self.neighbors = None
-        self.temperature = random.randint(0,50)
+        self.temperature = 20 - math.dist(self.center, self.c)
         self.switch = True
 
-
-    def check_state(self, center):
+        self.color = 'k'
+    def turn_on_switch(self):
+        # This only runs on the first time to see where to start
+        # if 19 < self.temperature < 21::
+        if self.temperature < 10:
+            self.switch = True
+        elif self.temperature >= 10:
+            self.switch = False
+            
+    def check_state(self):
         if self.switch:
-            self.cold(center)
+            self.im_cold()
         elif not self.switch:
-            self.warm(center)
+            self.im_warm()
         
     def penguin_in_front(self):
         for penguin in self.neighbors:        
@@ -32,46 +43,56 @@ class Penguin:
         return False
                 
     def move(self): 
-        if self.penguin_in_front():
-            self.x = -1 * self.change_c[0]
-            self.y = -1 * self.change_c[1]
-
-        else:
-            self.x = self.change_c[0]
-            self.y = self.change_c[1]
-
+        # if self.penguin_in_front():
+        #     self.x = -1 * self.change_c[0]
+        #     self.y = -1 * self.change_c[1]
+        #     self.c = np.array([self.x, self.y])
+        #     self.temperature =  self.temperature = 20 - math.dist(self.center, self.c)
+            
+        # else:
+        self.x = self.change_c[0]
+        self.y = self.change_c[1]
+        self.c = np.array([self.x, self.y])
+        self.temperature = 20 - math.dist(self.center, self.c)
+        
         self.change_x = 0
         self.change_y = 0
 
-    def cold(self, center): 
+    def im_cold(self): 
         # move to center
-        self.change_x = center[0] - self.x
-        self.change_y = center[1] - self.y
+        self.change_x = self.center[0] - self.x
+        self.change_y = self.center[1] - self.y
 
         new_x = self.x + (self.change_x * self.dt)   
         new_y = self.y + (self.change_y * self.dt)
 
         self.change_c = np.array([new_x, new_y])
-
-        self.temperature += 1
-
-        if self.temperature == 50:
+    
+        if 19 < self.temperature < 21:
             self.switch = False
     
-    def warm(self, center): 
+    def im_warm(self): 
         # move away from center
-        self.change_x = self.x - center[0]
-        self.change_y = self.y - center[1]
+        self.change_x = self.x - self.center[0]
+        self.change_y = self.y - self.center[1]
 
         new_x = self.x + (self.change_x * self.dt)   
         new_y = self.y + (self.change_y * self.dt)
 
         self.change_c = np.array([new_x, new_y])
 
-        self.temperature -= 1
-
-        if self.temperature == 0:
+        if self.temperature < 1:
             self.switch = True
+            
+    def change_colors(self):
+        if math.dist(self.c, self.center) <= 5:
+            self.color = 'tab:red'
+        elif 5 < math.dist(self.c , self.center) <= 10:
+            self.color = 'tab:orange'
+        elif 10 < math.dist(self.c, self.center) <= 20:
+            self.color = 'tab:blue'
+        else:
+            self.color = 'tab:cyan'
 
     def repel(self):
         r = 0
--- main.py
+++ main.py
@@ -1,49 +1,55 @@
 import matplotlib.pyplot as plt
+import matplotlib.colors as mcolors
 from agent import Penguin
 import random
 import numpy as np
 
 plotRealTime = True
 Nt = 1000
-min_pos = 75
-max_pos = 150
-num_agents = 20
-L = 200
+min_pos = 10
+max_pos = 40
+num_agents = 50
+L = 50
 
+def create_penguins():
+    # Creating agents 
+    penguins = [Penguin(random.randint(min_pos, max_pos), random.randint(min_pos, max_pos)) for _ in range(num_agents)]
 
-# Creating agents 
-penguins = [Penguin(random.randint(min_pos, max_pos), random.randint(min_pos, max_pos)) for _ in range(num_agents)]
-
-# Add neighbors to each penguin
-for penguin in penguins:
-    penguin.neighbors = [other_penguin for other_penguin in penguins if other_penguin is not penguin]
+    # Add neighbors to each penguin
+    for penguin in penguins:
+        penguin.neighbors = [other_penguin for other_penguin in penguins if other_penguin is not penguin]
+        penguin.turn_on_switch()
+        
+    return penguins
+        
+def main():
+    penguins = create_penguins()
     
-center = [L//2, L//2]
+    fig, ax = plt.subplots()
+    
+    for i in range(Nt):
 
-fig, ax = plt.subplots()
-for i in range(Nt):
+        x = [agent.x for agent in penguins]
+        y = [agent.y for agent in penguins]
+        colors = [agent.color for agent in penguins]
+    
+        for agent in penguins:
+            agent.check_state()
 
-    x = [agent.x for agent in penguins]
-    y = [agent.y for agent in penguins]
-   
-    for agent in penguins:
-        agent.check_state(center)
+        if plotRealTime or (i == Nt-1):
+            plt.cla()
+            # Add penguins
+            plt.scatter(x, y, s=60, c=colors)
+            plt.scatter(L//2, L//2, s=60, c='tab:brown')
+            ax.set(xlim=(0, L), ylim=(0, L))
+            ax.set_aspect('equal')
+            plt.pause(0.1)
 
-    if plotRealTime or (i == Nt-1):
-        plt.cla()
-        # Add penguins
-        plt.scatter(x, y, s=60, c='b')
-        # Add center
-        ax.scatter(center[0], center[1], s=60, c='y')
-        ax.set(xlim=(0, L), ylim=(0, L))
-        ax.set_aspect('equal')
-        plt.pause(0.01)
+        for agent in penguins:
+            agent.move()
+            agent.change_colors()
 
-    for agent in penguins:
-        agent.move()
-
-    chosen_penguin = penguins[0]
-
-    print(chosen_penguin.temperature)
-
-plt.show()
+    plt.show()
+    
+if __name__ == '__main__':
+    main()
